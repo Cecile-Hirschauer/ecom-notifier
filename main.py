4c180f1134617ef724c4a2002eeee3fd7afa8a05
cecile hirschauer <cecile.h.75@gmail.com>
2024-05-29 16:25:49 +0200

    Create scraping script to get product price - Add Brightdata proxy + Add Pushover notification when price decrease

--- main.py
+++ main.py
@@ -1,27 +1,110 @@
+import os
+import sys
+
+from datetime import datetime
+from pathlib import Path
+
+import json
 import requests
 from selectolax.parser import HTMLParser
 
+from dotenv import load_dotenv
+from loguru import logger
 
-def main(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+load_dotenv()
+
+FILE_PRICE = Path(__file__).parent / 'price.json'
+
+logger.remove()
+logger.add(sys.stderr, level="DEBUG")
+logger.add("logs/debug.log", level="WARNING", rotation="1 MB")
+
+
+def write_price_to_file(price: int):
+    logger.info(f"Writing price {price} to file")
+    if FILE_PRICE.exists():
+        with open(FILE_PRICE, 'r') as f:
+            data = json.load(f)
+    else:
+        data = []
+
+    data.append({
+        "price": price,
+        "timestamp": datetime.now().isoformat()
+    })
+
+    with open(FILE_PRICE, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+def get_price_differance(current_price: int) -> int:
+    logger.info("Getting price difference")
+    if FILE_PRICE.exists():
+        with open(FILE_PRICE, 'r') as f:
+            data = json.load(f)
+
+        previous_price = data[-1]['price']
+    else:
+        previous_price = current_price
+
+    try:
+        return round((previous_price - current_price) / previous_price * 100)
+    except ZeroDivisionError as e:
+        logger.error("previous_price value is zero: division by 0 impossible ")
+        raise e
+
+
+def send_alert(message: str):
+    logger.info(f"Sending alert: {message}")
+    try:
+        response = requests.post("https://api.pushover.net/1/messages.json",
+                                 data={
+                                     "token": os.environ["PUSHOVER_TOKEN"],
+                                     "user": os.environ["PUSHOVER_USER"],
+                                     "message": message,
+                                 })
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logger.error(f"Error sending alert: {str(e)}")
+        raise e
+
+
+def get_current_price(asin: str):
+    proxies = {
+        "http": os.environ['PROXY'],
+        "https": os.environ['PROXY']
     }
-    response = requests.get(url, headers=headers)
-    html_content = response.text
 
-    with open("amazon.html", "w") as f:
-        f.write(html_content)
+    url = f"https://www.amazon.fr/dp/{asin}"
+
+    try:
+        response = requests.get(url, proxies=proxies, verify=False)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        logger.error(f"Couldn't fetch content from {url} du to: {str(e)}")
+        raise e
+
+    html_content = response.text
 
     tree = HTMLParser(html_content)
     price_node = tree.css_first("span.a-price-whole")
-    return int(price_node.text().replace(",", ""))
+
+    error_msg = f"Couldn't find price in {url}"
+    if price_node:
+        return int(price_node.text().replace(",", ""))
+    logger.error(error_msg)
+    raise ValueError(error_msg)
+
+
+def main(asin: str):
+    current_price = get_current_price(asin=asin)
+    price_difference = get_price_differance(current_price=current_price)
+    write_price_to_file(price=current_price)
+
+    if price_difference > 0:
+        send_alert(f"Price has decreased by {price_difference}%")
 
 
 if __name__ == '__main__':
-    import time
-
-    asin = "B0B46N7QQL"
-    url = f"https://www.amazon.fr/dp/{asin}"
-    for i in range(10):
-        print(main(url))
-        time.sleep(0.5)
+    current_asin = "B0B46N7QQL"
+    main(current_asin)
